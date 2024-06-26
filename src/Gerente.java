71b124b8e4ed811f93930e68799abd666827cbff
HaralanS <haralansantana@gmail.com>
2024-06-26 17:15:45 -0300

    adiciona classe gerente

---
+++ src/Gerente.java
@@ -0,0 +1,56 @@
+public class Gerente {
+
+    public String nome;
+    private int idade;
+    private int tempoDeCasa;
+    private String setor;
+    private double salario;
+
+    public Gerente(double salario, String setor, int tempoDeCasa, int idade, String nome) {
+        this.salario = salario;
+        this.setor = setor;
+        this.tempoDeCasa = tempoDeCasa;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getTempoDeCasa() {
+        return tempoDeCasa;
+    }
+
+    public void setTempoDeCasa(int tempoDeCasa) {
+        this.tempoDeCasa = tempoDeCasa;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
